9629d03f1b207af2c22b2190960b9e290fd99f29
ajay36.an@gmail.com <ajay36.an@gmail.com>
2022-11-04 12:11:23 +0000

    different from answer

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ num = 0
 
 def generate_random_number():
   return random.randint(1,10)
-  
+
+
+def different_from_answer(guess, answer):
+  guess = int(guess)
+  answer = int(answer)
+  if guess == answer:
+    return 'Correct'
+  elif guess > answer:
+    return 'Too high'
+  elif guess < answer:
+    return 'Too low'
+    
